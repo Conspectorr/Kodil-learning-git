9545c537ea563a11c8c04ac2c0a3800e83708083
Paweł Kamiński <cien-kamien@wp.pl>
2022-05-09 10:15:14 +0200

    Add len product

--- Zadanie 3.2- lista zakupow znow.py
+++ Zadanie 3.2- lista zakupow znow.py
@@ -5,3 +5,8 @@ sklepy["warzywniak"]=['marchew','seler','rukola']
 
 for sklep, produkty in sklepy.items():
     print(f"Idę do {sklep.capitalize()}, kupuję tam {produkty}.")
+
+ilość=0
+for produkty in sklepy.values():
+    ilość=ilość+len(produkty)
+print(f"W sumie kupuję {ilość} produktów.")    

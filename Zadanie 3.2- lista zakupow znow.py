06daeaa595f796b5f439015a258d2183aa08fad0
Paweł Kamiński <cien-kamien@wp.pl>
2022-05-09 10:13:40 +0200

    Add loop for

--- Zadanie 3.2- lista zakupow znow.py
+++ Zadanie 3.2- lista zakupow znow.py
@@ -2,3 +2,6 @@ print("Lista zakupów")
 sklepy={}
 sklepy["piekarnia"]=['chleb','pączek','bułki']
 sklepy["warzywniak"]=['marchew','seler','rukola']
+
+for sklep, produkty in sklepy.items():
+    print(f"Idę do {sklep.capitalize()}, kupuję tam {produkty}.")
